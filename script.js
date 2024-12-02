876ea4ea91b470bafbca321d972673fdfe172c48
shivam-AI-1git config -global user.email shivamsinghnegi.codingita@gmail.com <shivamsinghnegi.codingita@gmail.com>
2024-12-02 12:18:59 +0530

    carousel 1.0

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-
 let currentIndex = 0;
 const slides = document.querySelectorAll(".carousel-slide img");
 const totalSlides = slides.length;
 
 function showSlide(index) {
   const carousel = document.querySelector(".carousel");
-  currentIndex = (index + totalSlides) % totalSlides;
+  currentIndex = (index + totalSlides) % totalSlides; 
   const offset = -currentIndex * 100; 
-  carousel.style.transform = `translateX(${offset}%)`;
+  carousel.style.transform = `translateX(${offset}%)`; 
 }
 
 function moveSlide(step) {
